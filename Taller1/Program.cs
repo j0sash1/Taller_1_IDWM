f2b9efd9198422fb6ab8343a97cad121833c13c9
j0sash1 <146682288+j0sash1@users.noreply.github.com>
2025-04-19 15:09:35 -0400

    Corrigiendo submódulo: dejando Taller1 como carpeta normal

---
+++ Taller1/Program.cs
@@ -0,0 +1,31 @@
+using Serilog;
+Log.Logger = new LoggerConfiguration()
+
+    .CreateLogger();
+
+try
+{
+    Log.Information("starting server.");
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddControllers();
+    builder.Host.UseSerilog((context, services, configuration) =>
+    {
+        configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithThreadId()
+            .Enrich.WithMachineName();
+    });
+
+    var app = builder.Build();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "server terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
