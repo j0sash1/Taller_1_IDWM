d70605fbca6718c779347ca54cb8fcef1aef0cb5
GustavoMails <gustavo.miles@alumnos.ucn.cl>
2025-05-06 20:50:36 -0400

    feat(Repositories): se agrego el repositorio para productos

---
+++ Taller1/Src/Repositories/ProductRepository.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Taller1.Src.Data;
+using Taller1.Src.Interfaces;
+using Taller1.Src.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Taller1.Src.Repositories
+{
+    public class ProductRepository(StoreContext store, Ilogger<Product> logger) : IProductRepository
+    {
+        private readonly StoreContext _context = store;
+        private readonly Ilogger<Product> _logger = logger;
+        public async Task AddProductAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+        }
+
+        public void DeleteProductAsync(Product product)
+        {
+            await _context.Products.Remove(product);
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id) ?? throw new Exception("Product not found");
+        }    
+
+        public async Task<IEnumerable<Product>> GetProductAsync()
+        {
+            return await _context.Products.ToListAsync() ?? throw new Exception("No products found");
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            var existingProduct = await _context.Product.FindAsync(product.Id) ?? throw new Exception("Product not found");
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.Price = product.Price;
+            existingProduct.Stock = product.Stock;
+            existingProduct.Urls = product.Urls;
+            existingProduct.Brand = product.Brand;
+            _context,Products.Update(existingProduct);
+        }   
+    }
+}
