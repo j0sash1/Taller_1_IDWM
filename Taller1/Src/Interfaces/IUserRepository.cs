1cc3d06dea9c61ccf17d02e3b249097b1b0b05f1
GustavoMails <gustavo.miles@alumnos.ucn.cl>
2025-05-06 22:46:40 -0400

    feat(Interfaces): se agrego una interface para UserRepository

--- Taller1/Src/Interfaces/IUserRepository.cs
+++ Taller1/Src/Interfaces/IUserRepository.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Taller1.Src.Dtos;
+using Taller1.Src.Models;
+
 namespace Taller1.Src.Interfaces
 {
     public interface IUserRepository
     {
+        Task<IEnumerable<UserDto>> GetAllUsersAsync();
+        Task<UserDto> GetUserByIdAsync(string firstName);
+        Task CreatedUserAsync(User user, ShippingAddres? shippingAddres);
+        void UpdateUserAsync(User user);
+        void UpdateShippingAddressAsync(UserDto userDto);
+        void DeleteUserAsync(User user, ShippingAddres shippingAddres);
 
     }
 }
