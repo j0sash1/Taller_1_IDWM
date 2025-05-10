d560375ead525b7a2272a4a69220212a8b18703e
GustavoMails <gustavo.miles@alumnos.ucn.cl>
2025-05-10 00:48:24 -0400

    fix(Dtos): se arreglaron errores de referencia en ShoppingCartDto

--- Taller1/Src/Dtos/ShoppingCartDto.cs
+++ Taller1/Src/Dtos/ShoppingCartDto.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Taller1.Src.Dtos
+
 namespace Taller1.Src.Dtos
 {
     public class ShoppingCartDto
     {
         public string CartId { get; set; } = string.Empty;
-        public List<ShoppingItemDto> Items { get; set; } = new();
+        public List<ShoppingItemDTo> Items { get; set; } = new();
         public decimal Total => Items.Sum(item => item.Price * item.Quantity); 
     }
 }
