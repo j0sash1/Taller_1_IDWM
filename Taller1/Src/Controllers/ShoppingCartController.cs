9f0d4a4ffb96d3d2471e3c3adcc18ff98946127c
GustavoMails <gustavo.miles@alumnos.ucn.cl>
2025-05-10 01:36:31 -0400

    feat(Controllers): se agregaro un controller para la clase ShoppingCart

---
+++ Taller1/Src/Controllers/ShoppingCartController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Taller1.Src.Interfaces
+
+[ApiController]
+[Route("api/[controller]")]
+namespace Taller1.Src.Controllers
+{
+    public class ShoppingCartController: ControllerBase
+    {
+        private readonly IShoppingCartService _cartService;
+        public ShoppingCartController(IShoppingCartService cartService)
+        {
+            _cartService = cartService;
+        }
+        [HttpGet("{cartId}")]
+        public async Task<IActionResult> GetCart(string cartId)
+        {
+            var cart = await _cartService.GetCartAsync(cartId);
+            return Ok(cart);
+        }
+        [HttpPost("add")]
+        public async Task<IActionResult> AddItem([FromBody] AddItemDto dto)
+        {
+            await _cartService.AddItemAsync(dto.CartId, dto.ProductId, dto.Quantity);
+            return NoContent();
+        } 
+    }
+}
