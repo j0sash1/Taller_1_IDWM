465d2e65179e544d8db71211e9aa2cb316241982
GustavoMails <gustavo.miles@alumnos.ucn.cl>
2025-05-10 01:07:57 -0400

    feature(Mappers): se agrego el mapper para la clase shoppingCart

---
+++ Taller1/Src/Mappers/ShoppingCartMapper.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller1.Src.Mappers
+{
+    public static class ShoppingCartMapper
+    {
+        return new ShoppingCartDto
+        {
+            CartId = ShoppingCartMapper.CartId,
+            Items = ShoppingCartMapper.Items.Select(item => new ShoppingItemDto
+            {
+                ProductId = item.ProductId,
+                ProductName = item.Product.Name,
+                Quantity = item.Quantity,
+                Price = item.Product.Price
+            }).ToList()
+        };
+    }
+}
