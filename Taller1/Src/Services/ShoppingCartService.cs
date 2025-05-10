5e0a6ad5408109f368a4c0b9cf116a3fff1f690a
GustavoMails <gustavo.miles@alumnos.ucn.cl>
2025-05-10 01:50:26 -0400

    refactor(Services): se agregaro la lógica para agregar un producto al carrito en la clase ShoppingCartService

--- Taller1/Src/Services/ShoppingCartService.cs
+++ Taller1/Src/Services/ShoppingCartService.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Taller1.Src.Data;
+using Taller1.Src.Dtos;
+using Taller1.Src.Interfaces;
+using Taller1.Src.Mappers;
+using Taller1.Src.Models;
+
 namespace Taller1.Src.Services
 {
     public class ShoppingCartService: IShoppingCartService
@@ -19,7 +25,23 @@ namespace Taller1.Src.Services
         }
         public async Task AddItemAsync(string cartId, int productId, int quantity)
         {
-        // Implementar lógica para añadir producto al carrito
+            if (quantity <= 0)
+            {
+                throw new ArgumentException("La cantidad debe ser mayor a 0", nameof(quantity));
+            }
+            var cart = await _unitOfWork.ShoppingCartRepository.GetByCartIdAsync(cartId);
+            if (cart == null)
+            {
+                cart = new ShoppingCart { CartId = cartId }
+                await _unitOfWork.ShoppingCartRepository.AddAsync(cart);
+            }
+            var product = await _unitOfWork.ProductRepository.GetProductByIdAsync(productId);
+            if (product == null)
+            {
+                throw new ArgumentException("Producto no encontrado", nameof(productId));
+            }
+            cart.AddItem(product, quantity);
+            await _unitOfWork.CompleteAsync();
         }
         public async Task RemoveItemAsync(string cartId, int productId, int quantity)
         {
